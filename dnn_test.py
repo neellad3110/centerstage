c8d889ea03daa9e24f8ef993bcf73b3688050698
Neel Lad <neellad@Neels-MacBook-Air.local>
2023-07-18 21:47:49 +0530

    implemented dnn face detection algorithm

--- dnn_test.py
+++ dnn_test.py
@@ -1,56 +1,30 @@
+from imutils import face_utils
+import dlib
 import cv2
 
-# Load the pre-trained model
-model_path = "deploy.prototxt"
-#weights_path = "model.caffemodel"
-weights_path = "model.xml"
-try:
-    net = cv2.dnn.readNet(model_path, weights_path)
-except cv2.error as e:
-    print(f"Error loading model files: {e}")
-    exit()
 
-# Initialize the video capture
-cap = cv2.VideoCapture(0)
+face_detect = dlib.get_frontal_face_detector()
+
+video_capture = cv2.VideoCapture(0)
+flag = 0
 
 while True:
-    # Read frame from the webcam
-    ret, frame = cap.read()
 
-    # Resize frame for faster processing
-    frame = cv2.resize(frame, None, fx=0.6, fy=0.6)
+    ret, frame = video_capture.read()
 
-    # Convert frame to blob for input to the network
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=True, crop=False)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = face_detect(gray, 1)
 
-    # Set the input to the network
-    net.setInput(blob)
+    for (i, rect) in enumerate(rects):
 
-    # Perform face detection
-    detections = net.forward()
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
 
-    # Process the detections
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        frame=cv2.flip(frame,1)
+        cv2.imshow('Video', frame)
 
-        # Filter out weak detections
-        if confidence > 0.5:
-            # Get the coordinates of the bounding box
-            box = detections[0, 0, i, 3:7] * frame.shape[1]
-            (startX, startY, endX, endY) = box.astype(int)
-
-            # Draw the bounding box and confidence
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            text = f"Confidence: {confidence:.2f}"
-            cv2.putText(frame, text, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-
-    # Display the frame
-    cv2.imshow("Face Detection", frame)
-
-    # Exit if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture and close the windows
-cap.release()
+video_capture.release()
 cv2.destroyAllWindows()
