0c6ca980ea59fb5f9b856aa48636fc93aaf1cde8
Neel Lad <neellad@Neels-MacBook-Air.local>
2023-09-11 13:45:42 +0530

    final center stage

--- facedetection_zoom.py
+++ facedetection_zoom.py
@@ -1,16 +1,13 @@
-# importing the required libraries
 import cv2
 import math
 import numpy as np
-from scipy.ndimage import map_coordinates
-from scipy.ndimage import shift
 import time
 
 
-# Define the video capture source (0 for webcam)
+
 video_source = 0
 image=[]
-# Initialize video capture
+
 cap = cv2.VideoCapture(video_source)
 
 # Check if the camera/video file is opened successfully
@@ -44,25 +41,14 @@ def move_background_with_custom_ratio(original_frame, face_bbox, ratio_top_botto
     target_center_x = frame_width / 2
     target_center_y = frame_height / 2
 
-    background_movement_x = target_center_x - face_center_x
-    background_movement_y = target_center_y - face_center_y
+    background_movement_x = (target_center_x - face_center_x) * (ratio_left_right / 100)
+    background_movement_y = (target_center_y - face_center_y) * (ratio_top_bottom / 100)
 
-    # Scale the background movement according to the specified ratios
-    background_movement_x *= ratio_left_right / 100
-    background_movement_y *= ratio_top_bottom / 100
+    # Define the affine transformation matrix
+    translation_matrix = np.float32([[1, 0, background_movement_x], [0, 1, background_movement_y]])
 
-    # Create a grid of coordinates for shifting
-    x_coords = np.arange(frame_width)
-    y_coords = np.arange(frame_height)
-    X, Y = np.meshgrid(x_coords, y_coords, indexing='ij')
-
-    # Perform background movement using scipy.ndimage.shift
-    shifted_x = X - background_movement_x
-    shifted_y = Y - background_movement_y
-
-    background_shifted = np.zeros_like(original_frame)
-    for channel in range(original_frame.shape[2]):
-        background_shifted[:, :, channel] = shift(original_frame[:, :, channel], (background_movement_y, background_movement_x), mode='reflect')
+    # Apply the affine transformation using OpenCV
+    background_shifted = cv2.warpAffine(original_frame, translation_matrix, (frame_width, frame_height), borderMode=cv2.BORDER_REFLECT)
 
     return background_shifted
 
@@ -73,75 +59,49 @@ haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 cam = cv2.VideoCapture(0)
 latest_position=[]
 
-
-
+last_shift_time = time.time()
+background_shifted=None
+transition_frames=10
 while True:
     _, image = cam.read()
 
-   
-   
-
-    #convert each frame from BGR to Grayscale
     grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # detect faces using Haar Cascade 
     face = haar_cascade.detectMultiScale(image, 1.3, 5)
 
-    if(len(image)>0):
-
-        # draw a rectangle around the face and update the text to Face Detected
+    if len(face) > 0:
         for (x, y, w, h) in face:
-
-            
-
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            #cv2.rectangle(image, (1050,110), (250, 630), (0, 255, 0), 2)
-
-            center_x, center_y = x + w // 2, y + h // 2
-            #sCalculate the distance from the screen edges to the face center
-            # screen_center_x, screen_center_y = image.shape[1] // 2, image.shape[0] // 2
-            
-            # distance_left = calculate_distance((0, center_y), (center_x, center_y))  # Distance to left side
-            # distance_right = calculate_distance((image.shape[1] - 1, center_y), (center_x, center_y))  # Distance to right side
-            # distance_top = calculate_distance((center_x, 0), (center_x, center_y))  # Distance to top side
-            # distance_bottom = calculate_distance((center_x, image.shape[0] - 1), (center_x, center_y))  # Distance to bottom side
-
-
-            # Draw lines from the screen edges to the face center
-            # cv2.line(image, (0, center_y), (center_x, center_y), (0, 255, 0), 2)          # Left side
-            # cv2.line(image, (image.shape[1]-1, center_y), (center_x, center_y), (0, 255, 0), 2)  # Right side
-            # cv2.line(image, (center_x, 0), (center_x, center_y), (0, 255, 0), 2)          # Top side
-            # cv2.line(image, (center_x, image.shape[0]-1), (center_x, center_y), (0, 255, 0), 2)  # Bottom side
-            
             face_bbox = [x, y, w, h]
-            
-            
-            move_timer = time.localtime() # get struct_time
-            time_string = int(time.strftime("%S", move_timer))
-            
-            if time_string % 5 == 0:
 
-                image=move_background_with_custom_ratio(image,face_bbox)
-                latest_position=face_bbox
-            
+            current_time = time.time()
+            if current_time - last_shift_time >= 5:
+                last_shift_time = current_time
+                image = move_background_with_custom_ratio(image, face_bbox)
+                latest_position = face_bbox
+
+            elif background_shifted is not None:
+                if transition_frames > 0:
+                    alpha = 1.0 - (transition_frames / 15)  # Calculate interpolation factor
+                    blended = cv2.addWeighted(image, alpha, background_shifted, 1 - alpha, 0)
+                    transition_frames -= 1
+                    image = blended
+                else:
+                    background_shifted = move_background_with_custom_ratio(background_shifted, latest_position)
+                    image = background_shifted.copy()
+
+                   
             else:
-                if(len(latest_position)>0):
-                    image=move_background_with_custom_ratio(image,latest_position)
-               
-            
-          
-            #  # Extract the region of interest from the frame
-            
-            image = image[roi_y1:roi_y2, roi_x1:roi_x2]
-    else :
-            image = image[roi_y1:roi_y2, roi_x1:roi_x2]
-        
+                if len(latest_position) > 0:
+                    image = move_background_with_custom_ratio(image, latest_position)
 
-    image=cv2.flip(image,1)
+            image = image[roi_y1:roi_y2, roi_x1:roi_x2]
+    else:
+        image = image[roi_y1:roi_y2, roi_x1:roi_x2]
+
+    image = cv2.flip(image, 1)
+    
     cv2.imshow("Center Stage", image)
     key = cv2.waitKey(10)
 
-    # Press `q` to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
